0b883865a86985730ca053254b146d8dbf7f20ce
Sushant Sikka <sushantsikka.506@gmail.com>
2017-04-05 19:16:21 +0530

    Update SendEmail.aspx.cs

--- MathOperations/SendEmail.aspx.cs
+++ MathOperations/SendEmail.aspx.cs
@@ -18,11 +18,11 @@ namespace DatabaseDiary
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-            MailMessage mail = new MailMessage("sushantsikka.506@gmail.com", TextBox1.Text, TextBox2.Text, TextBox3.Text);
+            MailMessage mail = new MailMessage("[email-id@host.com]", TextBox1.Text, TextBox2.Text, TextBox3.Text);
             SmtpClient smtp = new SmtpClient();
             smtp.Host = "smtp.gmail.com";
             smtp.EnableSsl = true;
-            System.Net.NetworkCredential NetworkCred = new System.Net.NetworkCredential(TextBox1.Text, "Charminar@123");
+            System.Net.NetworkCredential NetworkCred = new System.Net.NetworkCredential(TextBox1.Text, [password]);
             smtp.UseDefaultCredentials = true;
            // SmtpDeliveryMethod.Network;
             smtp.Port = 587;
